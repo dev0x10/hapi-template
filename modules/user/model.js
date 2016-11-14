c0580d3788421df4f9eb4ceb76b468fb1ffa9819
dev0x10 <dev.yauri@gmail.com>
2016-11-14 23:02:51 +0800

    clean up

--- modules/user/model.js
+++ modules/user/model.js
@@ -6,29 +6,21 @@ userModel.members = [
   {id: 1, fName: 'Joker', lName: 'Breaker', age: 33}
 ];
 
-userModel.get = (id) => userModel.members.map((m) => {
-  if (m.id == id) {
-    return m;
-  }
-
-  return {};
-}).pop();
+userModel.get = (id) => userModel.members.filter((m) => m.id === id).pop() || {};
 
 userModel.create = (user) => {
-
   let newUserObj = {
-    fName: Joi.string(),
-    lName: Joi.string(),
+    fName: Joi.string().required(),
+    lName: Joi.string().required(),
     age: Joi.number().positive()
   };
 
-  if (commonUtil.isValidObject(user, newUserObj)) {
-    user.id = userModel.members.length + 1;
-    this.members.push(user);
-    return true;
-  } else {
-    return false;
+  if (!commonUtil.isValidObject(user, newUserObj)) {
+    return {};
   }
+  user.id = userModel.members.length + 1;
+  this.members.push(user);
+  return user;
 };
 
 module.exports = userModel;
